136bde237b9cd33bf3555cc5102e50eb7a5f9bc1
harshad1924 <chavanharshad550@gmail.com>
2026-06-15 16:05:55 +0530

    Add files via upload

---
+++ FBS/DEMO/Basics/Variable.py
@@ -0,0 +1,5 @@
+x = "ABC"
+a = "XYZ"
+res = x + a
+res = 8 ** 8
+print(res)
---
+++ FBS/DEMO/Branching/if_else_ladder_demo.py
@@ -0,0 +1,7 @@
+num = int(input('Enter a number: '))
+if(num == 0):
+    print('Number is neutral')
+elif(num > 0):
+    print('Number is positive')
+else:
+    print('Number is negative')
